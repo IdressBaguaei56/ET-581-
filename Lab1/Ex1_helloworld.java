e6757dc0ffd6c18ed1bacf2082f00c1aa2b98cd6
IdressBaguaei56 <Idress.Baguaei56@student.qcc.cuny.edu>
2025-08-27 00:30:41 +0000

    save Hello World

---
+++ Lab1/Ex1_helloworld.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class Ex1_helloworld {
+    
+    public static void main(String[]args) {
+        System.out.print("This is not a new line");
+        System.out.println("Hello World!");
+        System.out.print("This is not a new line");
+    }
+}
