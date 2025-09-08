a18726773fff3874a5c203c7c8b966cdf195b322
Idress Baguaei <Idress.Baguaei56@student.qcc.cuny.edu>
2025-09-08 10:20:53 -0400

    add lab 2

---
+++ Lab2/MyStringTokenizer.java
@@ -0,0 +1,21 @@
+import java.util.StringTokenizer;
+
+public class MyStringTokenizer{
+    public static void main(String[] args){
+        StringTokenizer st1 = new StringTokenizer("apple.banana.orange mango")
+        System.out.println(st1.nextToken());
+        System.out.println(st1.nextToken());
+        System.out.println("============================");
+
+        StringTokenizer st2 = new StringTokenizer("apple.banana.orange mango", ".")
+        System.out.println(st2.nextToken());
+        System.out.println(st2.nextToken());
+        System.out.println(st2.nextToken());
+
+        StringTokenizer st3 = new StringTokenizer("apple.banana.orange mango", ". ")
+        System.out.println(st3.nextToken());
+        System.out.println(st3.nextToken());
+        System.out.println(st3.nextToken());
+
+    }
+}
---
+++ Lab2/SubStringExample.java
@@ -0,0 +1,7 @@
+public class SubStringExample{
+    public static void main(String[] args){
+        String str1 = "Hello New York City";
+        System.out.println("Print out New York City using .substring()")
+
+    }
+}
